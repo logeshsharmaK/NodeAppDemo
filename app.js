5b0e3f78904b546575e393199be6312fed799cda
logeshsharmaK <64588164+logeshsharmaK@users.noreply.github.com>
2021-10-04 18:59:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,106 +2,84 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-
 const testUsers = {}
-
 const app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.get('/ping', (req, res, next) => {
   res.send('pong')
 });
-
 app.post('/user/verify', (req, res, next) => {
   const token = req.body.token
-
   console.log("POST /user/verify. token: ", token);
-
   const userData = verifyUser(token)
-
   console.log("userData", userData);
-
   if (!userData) {
     return res.status(422).json({error: "User token is invalid"})
   }
   return res.status(200).json(userData)
 });
-
 app.get('/user/verify', (req, res, next) => {
-  const token = req.body.token
-
+  const token = req.query.token
   console.log("GET /user/verify. token: ", token);
-
   const userData = verifyUser(token)
-
   console.log("userData", userData);
-
   if (!userData) {
     return res.status(422).json({error: "User token is invalid"})
   }
   return res.status(200).json(userData)
 });
-
 function verifyUser(token) {
-
   // TODO:
   // here basically we need to verify a provided data (e.g. token)) against DB
   // and if the provided token is valid -> return a data with specified format back to ConnectCube server,
   // as described here https://developers.connectycube.com/guides/custom-identity-provider
-
   const isValid = isTokenValid(token);
   if (!isValid) {
     return null
   }
-
   let userData = testUsers[token]
   if (!userData) {
-	console.log("token--------------->", token);
-	  
-    const userID = Math.floor(Math.random() * 10000);
+	//  let randomMultiples
+	  //if(token == "1"){
+	//	  randomMultiples = 1000000
+	  //}else {
+		//  randomMultiples = 10000
+	//  }
+	  console.log("token--------------->", token);
+
+    const userID = Math.floor(Math.random() * 100000);
 
     const uLogin = ""+userID;
     const uEmail = userID + "@test.com";
     const uFullName = "Logesh sharma"+userID ;
-
     userData = {
         uid: userID,
       login: uLogin,
       email: uEmail,
       fullName: uFullName,
     };
-
     testUsers[token] = userData
   }
-
   return userData;
 }
-
 function isTokenValid(token) {
   // TODO:
   // here basically we need to verify a provided data (e.g. token)) against DB
-
   // For a test purpose we will be returning true all the time
-
   return true
 }
-
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
-
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.sendStatus(err.status || 500);
 });
-
 module.exports = app;
